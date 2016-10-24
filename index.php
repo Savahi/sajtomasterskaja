c47d7c204da89bde5aac2efec7fd0e0b09296f67
Savahi <hirshhorn@yahoo.com>
2016-10-24 22:20:31 +0300

    edited index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+﻿<!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
+	<META NAME="DESCRIPTION" CONTENT="Сайтомастерская: создание сайтов">
 
+	<META NAME="KEYWORDS" CONTENT="создание сайтов, дешево, недорого, Wordpress, CodeIgniter">
 
 	<title>
+		Сайтомастерская: создание веб-сайтов любой сложности. Продвижение	
 	</title>
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -26,6 +30,7 @@
 
 			
 			<div class="page-header">
+				<h1>Услуги по созданию и продвижению веб-сайтов</h1>
 			</div>
 
 			<div class='container-fluid'>
@@ -33,11 +38,17 @@
 					<div class="col-md-4 text-muted">
 						<div class="panel panel-default">
     						<div class="panel-heading">
+								<h4>Сайт-визитка</h4>
 								<div style='font-size:115%; font-weight:bold;'>2.500 &#8381;</div>
 							</div>
 						    <div class="panel-body">
+						    	Сайт-визитка "под ключ":
+						    	регистрация домена, выбор из нескольких тысяч готовых вариантов (адаптивного) дизайна, 
+						    	размещение сайта на хостинг-площадке, 5 статических страниц.
+						    	<p class='myWithTopMargin'><i>Стек технологий</i>: WordPress.</p>
 						    	<!--
 						    	<div style='margin-top:4px; border-top:1px solid lightgray; padding-top:4px; font-size:85%; font-style:italic;'>
+						    		Цена действительна для доменов в зоне "ru" или "рф".
 						    	</div>
 						    	-->
 							</div>
@@ -46,18 +57,33 @@
 					<div class="col-md-4 text-muted">
 						<div class="panel panel-default">
     						<div class="panel-heading">
+    							<h4>Сайт компании</h4>
 								<div style='font-size:115%; font-weight:bold;'>5.000 &#8381;</div>
 							</div>
 						    <div class="panel-body">
+						    	Сайт-компании "под ключ": 
+						    	регистрация домена, выбор варианта (адаптивного) дизайна, 
+						    	размещение сайта на хостинг-площадке, 20 статических страниц 
+						    	(+&nbsp;интерактивная карта со схемой проезда, форма обратной связи, корпоративная почта и прочее).<br>
+						    	<p class='myWithTopMargin'><i>Стек технологий</i>: WordPress или Drupal, Google&nbsp;Apps&nbsp;/&nbsp;Yandex&nbsp;API.</p>
 							</div>
 						</div>
 					</div>
 					<div class="col-md-4 text-muted">
 						<div class="panel panel-default">
     						<div class="panel-heading">
+    							<h4>Веб-портал</h4>
+    							<p>цена договорная</p>
 							</div>
 						    <div class="panel-body">
+						    	Веб-портал с системой личных кабинетов пользователей: 
+						    	регистрация домена, выбор дизайна (или верстка оригинального), 
+						    	размещение сайта на хостинг-площадке, 
+						    	статические и динамические страницы, состав и функциональность которых определяется 
+						    	нуждами конкретного клиента.<br>
 						    	<p class='myWithTopMargin'>
+						    		<i>Стек технологий</i> может включать: WordPress, CodeIgniter, Laravel, 
+							    	Yandex API, Google API и проч.
 							    </p>
 							</div>
 						</div>
@@ -71,9 +97,16 @@
 					<div class="col-md-4 text-muted">
 						<div class="panel panel-default">
     						<div class="panel-heading">
+								<h4>Веб-приложения</h4>
+								<p>цена договорная</p>
 							</div>
 						    <div class="panel-body">
+						    	Примеры веб-приложений: поиск запрошенной пользователем информации в базе данных сайта и выдача ее на экран,
+						    	отображение объектов на географической карте, онлайн-переводчик, калькулятор для расчета взносов по 
+						    	кредиту, онлайн-игра.<br>
 						    	<p class='myWithTopMargin'>
+						    		<i>Стек технологий</i> может включать: JavaScript, 
+						    		PHP, Perl, MySQL, GoogleAPI, YandexAPI и др.
 						    	</p>
 							</div>
 						</div>
@@ -81,9 +114,15 @@
 					<div class="col-md-4 text-muted">
 						<div class="panel panel-default">
     						<div class="panel-heading">
+    							<h4>Приложения для мобильных</h4>
+								<p>цена договорная</p>
 							</div>
 						    <div class="panel-body">
+						    	Иногда для удовлетворения специфических потребностей пользователей бывает необходимо создать 
+						    	взаимодействующее с сайтом мобильное приложение, которое может предоставлять пользователю больше удобста и/или 
+						    	функционала...<br>
 						    	<p class='myWithTopMargin'>
+						    		<i>Стек технологий</i>: PhoneGap или AndroidStudio+Java.
 						    	</p>
 							</div>
 						</div>
@@ -91,9 +130,16 @@
 					<div class="col-md-4 text-muted">
 						<div class="panel panel-default">
     						<div class="panel-heading">
+    							<h4>Продвижение</h4>
+    							<p>цена договорная</p>
 							</div>
 						    <div class="panel-body">
+						        Давно прошли те времена, когда для того, чтобы получить посетителей на сайт, было достаточно его просто создать!
+						        Продвижение сайта часто является необходимым условием успешного развития бизнеса.
 						        <p class='myWithTopMargin'>
+						        	Основные направления работ по продвижению: 
+						    		SEO-оптимизация и продвижение контентом, реклама в Yandex-direct и Google AdWords,
+						    		продвижение в социальных сетях.
 						    	</p>
 							</div>
 						</div>
