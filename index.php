b3b985d86d8b9d21e62beb76296af56fdef05d39
Savahi <hirshhorn@yahoo.com>
2016-10-24 22:15:14 +0300

    edited index.php

--- index.php
+++ index.php
@@ -1,5 +1,106 @@
-<?php
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-echo "HELLO!!"
 
-?>
+
+	<title>
+	</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+	<!--
+	<script src="/jquery/jquery-3.1.0.min.js"></script>
+	<script src="/bootstrap/js/bootstrap.min.js"></script>
+	-->
+	<style>
+		body { padding-top:50px; }
+
+		.myWithTopMargin { margin-top:1%; }
+	</style>
+
+</head>
+
+<body>
+
+			
+			<div class="page-header">
+			</div>
+
+			<div class='container-fluid'>
+				<div class="row">
+					<div class="col-md-4 text-muted">
+						<div class="panel panel-default">
+    						<div class="panel-heading">
+								<div style='font-size:115%; font-weight:bold;'>2.500 &#8381;</div>
+							</div>
+						    <div class="panel-body">
+						    	<!--
+						    	<div style='margin-top:4px; border-top:1px solid lightgray; padding-top:4px; font-size:85%; font-style:italic;'>
+						    	</div>
+						    	-->
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-muted">
+						<div class="panel panel-default">
+    						<div class="panel-heading">
+								<div style='font-size:115%; font-weight:bold;'>5.000 &#8381;</div>
+							</div>
+						    <div class="panel-body">
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-muted">
+						<div class="panel panel-default">
+    						<div class="panel-heading">
+							</div>
+						    <div class="panel-body">
+						    	<p class='myWithTopMargin'>
+							    </p>
+							</div>
+						</div>
+					</div>
+<!--
+				</div>
+			</div>
+			<div class='container-fluid'>
+				<div class="row">
+-->
+					<div class="col-md-4 text-muted">
+						<div class="panel panel-default">
+    						<div class="panel-heading">
+							</div>
+						    <div class="panel-body">
+						    	<p class='myWithTopMargin'>
+						    	</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-muted">
+						<div class="panel panel-default">
+    						<div class="panel-heading">
+							</div>
+						    <div class="panel-body">
+						    	<p class='myWithTopMargin'>
+						    	</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-muted">
+						<div class="panel panel-default">
+    						<div class="panel-heading">
+							</div>
+						    <div class="panel-body">
+						        <p class='myWithTopMargin'>
+						    	</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+</body>
+
+</html>
